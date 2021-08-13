08ca3bf28435b0a91cd17a5f52823b227af14217
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2021-08-13 19:35:33 -0400

    get channel subscriber count
    update

--- src/main/java/me/kavin/piped/utils/obj/Channel.java
+++ src/main/java/me/kavin/piped/utils/obj/Channel.java
@@ -5,16 +5,18 @@ import java.util.List;
 public class Channel {
 
     public String id, name, avatarUrl, bannerUrl, description, nextpage;
+    public long subscriberCount;
     public List<StreamItem> relatedStreams;
 
     public Channel(String id, String name, String avatarUrl, String bannerUrl, String description, String nextpage,
-            List<StreamItem> relatedStreams) {
+            long subscriberCount, List<StreamItem> relatedStreams) {
         this.id = id;
         this.name = name;
         this.avatarUrl = avatarUrl;
         this.bannerUrl = bannerUrl;
         this.description = description;
         this.nextpage = nextpage;
+        this.subscriberCount = subscriberCount;
         this.relatedStreams = relatedStreams;
     }
 }
