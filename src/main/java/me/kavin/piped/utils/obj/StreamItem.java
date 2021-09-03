15240855776802d17825ca99536dacb9417cbacd
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-09-03 23:05:58 +0530

    Rename field to fix consistency issues.

--- src/main/java/me/kavin/piped/utils/obj/StreamItem.java
+++ src/main/java/me/kavin/piped/utils/obj/StreamItem.java
@@ -2,18 +2,18 @@ package me.kavin.piped.utils.obj;
 
 public class StreamItem {
 
-    public String url, title, thumbnail, uploaderName, uploaderUrl, uploaderAvatarUrl, uploadedDate;
+    public String url, title, thumbnail, uploaderName, uploaderUrl, uploaderAvatar, uploadedDate;
     public long duration, views;
     public boolean uploaderVerified;
 
     public StreamItem(String url, String title, String thumbnail, String uploaderName, String uploaderUrl,
-            String uploaderAvatarUrl, String uploadedDate, long duration, long views, boolean uploaderVerified) {
+            String uploaderAvatar, String uploadedDate, long duration, long views, boolean uploaderVerified) {
         this.url = url;
         this.title = title;
         this.thumbnail = thumbnail;
         this.uploaderName = uploaderName;
         this.uploaderUrl = uploaderUrl;
-        this.uploaderAvatarUrl = uploaderAvatarUrl;
+        this.uploaderAvatar = uploaderAvatar;
         this.uploadedDate = uploadedDate;
         this.duration = duration;
         this.views = views;
