4a3a4236c00f3fc5c7c8502ba2dd631ee0409598
Kavin <20838718+FireMasterK@users.noreply.github.com>
2023-11-28 01:20:04 +0000

    Only enable ump for now.

--- src/main/java/me/kavin/piped/utils/CollectionUtils.java
+++ src/main/java/me/kavin/piped/utils/CollectionUtils.java
@@ -44,8 +44,7 @@ public class CollectionUtils {
         boolean livestream = info.getStreamType() == StreamType.LIVE_STREAM;
 
         final var extraParams = Map.of(
-                "ump", "1",
-                "srfvp", "1"
+                "ump", "1"
         );
 
         if (!livestream) {
